56506db7af05f197dde50de01a95acee384219e8
mbowen13 <mbowen@utexas.edu>
2018-04-14 14:07:57 -0500

    Create skeleton methods

--- src/AES.java
+++ src/AES.java
@@ -5,10 +5,17 @@ import static java.lang.System.*;
 public class AES {
 
 	private static int keySize; // 128 or 256
-	private static String mode; //encrypt or decrypt
 	private static FileInputStream keyFile;
     private static FileInputStream inputFile;
     private static FileInputStream outputFile;
+    private static String mode; //encrypt or decrypt
+    
+    private int numberOfRounds;
+    private int numberofKeys;
+    
+    // for testing
+    private static int testKey[] = new int[]{1,2,3,4,5,6,7,8,9,10,11,12,1,3,14,15,16};
+    private static String testInput = "This is test."; // 16 bytes
 	
 	
 	public static void readArgs(String[] args) {
@@ -22,6 +29,19 @@ public class AES {
             exit(1);
         }
 	}
+	
+	public AES(int[] key) {
+		// key expansion
+		// add round key
+		numberOfRounds = 1;	
+	}
+	
+	public static void subBytes() {}
+	public static void shiftRows() {}
+	public static void mixColumns() {}
+	public static void addRoundKey() {}
+	public static void keyExpansion() {}
+	
 	
 	public static void main(String[] args) throws IOException {
         out.print("start");
